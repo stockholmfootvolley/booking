e43bd2df809d35a88e96b00b922445862be5e1c4
Ramon Medeiros <ramon.medeiros@carla.se>
2022-09-29 08:31:05 +0200

    Beginner -> Basic

--- internal/pkg/model/model.go
+++ internal/pkg/model/model.go
@@ -14,34 +14,34 @@ const (
 )
 
 const (
-	Beginner Level = iota
+	Basic Level = iota
 	Medium
 	Advanced
 )
 
 func (l Level) String() string {
 	switch l {
-	case Beginner:
-		return "BEGINNER"
+	case Basic:
+		return "BASIC"
 	case Medium:
 		return "MEDIUM"
 	case Advanced:
 		return "ADVANCED"
 	default:
-		return "BEGINNER"
+		return "BASIC"
 	}
 }
 
 func StringToLevel(s string) Level {
 	switch s {
-	case Beginner.String():
-		return Beginner
+	case Basic.String():
+		return Basic
 	case Medium.String():
 		return Medium
 	case Advanced.String():
 		return Advanced
 	default:
-		return Beginner
+		return Basic
 	}
 }
 
