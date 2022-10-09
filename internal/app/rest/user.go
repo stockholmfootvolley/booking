61bc7c0bc5cd69755c66fcc907778a545d6d527d
Ramon Medeiros <ramon.medeiros@carla.se>
2022-10-09 15:34:56 +0200

    Don't set cookie on /user

--- internal/app/rest/user.go
+++ internal/app/rest/user.go
@@ -1,9 +1,7 @@
 package rest
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stockholmfootvolley/booking/internal/pkg/spreadsheet"
@@ -15,13 +13,5 @@ type UserInfo struct {
 }
 
 func (s *Server) getUser(c *gin.Context) {
-	userInfo := s.GetUserFromContext(c)
-
-	jsonContent, err := json.Marshal(&userInfo)
-	if err != nil {
-		c.AbortWithStatus(http.StatusUnprocessableEntity)
-	}
-
-	maxAge := time.Now().Add(time.Hour)
-	c.SetCookie("user", string(jsonContent), int(maxAge.Unix()), "/", c.Request.Host, true, false)
+	c.IndentedJSON(http.StatusOK, s.GetUserFromContext(c))
 }
